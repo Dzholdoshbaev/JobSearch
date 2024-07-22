f309aa505cfac434c59622c5ea83946f54799cbb
abdyrakhman <medzholdoshbaev@gmail.com>
2024-07-22 15:11:33 +0600

    added ContactTypes  Dao Dto Service ServiceIml Controller and MODEL

---
+++ src/main/java/dzholdoshbaev/jobsearch/model/ContactTypes.java
@@ -0,0 +1,9 @@
+package dzholdoshbaev.jobsearch.model;
+
+import lombok.Data;
+
+@Data
+public class ContactTypes {
+    private int id;
+    private String type;
+}
---
+++ src/main/java/dzholdoshbaev/jobsearch/service/impl/ContactTypesServiceImpl.java
@@ -0,0 +1,8 @@
+package dzholdoshbaev.jobsearch.service.impl;
+
+import dzholdoshbaev.jobsearch.service.ContactTypesService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ContactTypesServiceImpl implements ContactTypesService {
+}
