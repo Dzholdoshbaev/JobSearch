28928b1e44d2186c362474614928c7805fe58df1
abdyrakhman <medzholdoshbaev@gmail.com>
2024-07-25 16:02:10 +0600

    created SQL request methods for ResumesDao

--- src/main/java/dzholdoshbaev/jobsearch/dao/ResumesDao.java
+++ src/main/java/dzholdoshbaev/jobsearch/dao/ResumesDao.java
@@ -1,14 +1,33 @@
 package dzholdoshbaev.jobsearch.dao;
 
 import dzholdoshbaev.jobsearch.model.Resumes;
+import dzholdoshbaev.jobsearch.model.Vacancies;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Data
+@RequiredArgsConstructor
 public class ResumesDao {
-    private List<Resumes> resumesList = new ArrayList<>();
+    private final JdbcTemplate jdbcTemplate;
+
+    public List<Resumes> getAllResumes() {
+        String sql = "select * from resumes";
+        return jdbcTemplate.query(sql,new BeanPropertyRowMapper<>(Resumes.class));
+    }
+
+    public List<Resumes> getAllResumesByCategory(int categoryId) {
+        String sql = "SELECT * FROM resumes where category_id = ?";
+        return jdbcTemplate.query(sql,new BeanPropertyRowMapper<>(Resumes.class),categoryId);
+    }
+
+    public List<Resumes> getAllResumesByUser(int applicantId) {
+        String sql = "SELECT * FROM resumes where applicant_id = ?";
+        return jdbcTemplate.query(sql,new BeanPropertyRowMapper<>(Resumes.class),applicantId);
+    }
 }
