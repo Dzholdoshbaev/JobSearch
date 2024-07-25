fd3b18f6842f5087c75ce9da61fc343deb2d044f
abdyrakhman <medzholdoshbaev@gmail.com>
2024-07-25 16:01:12 +0600

    created SQL request methods

--- src/main/java/dzholdoshbaev/jobsearch/dao/CategoriesDao.java
+++ src/main/java/dzholdoshbaev/jobsearch/dao/CategoriesDao.java
@@ -1,13 +1,24 @@
 package dzholdoshbaev.jobsearch.dao;
 
+import dzholdoshbaev.jobsearch.model.Users;
 import jdk.jfr.Category;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 @Component
-@Data
+@RequiredArgsConstructor
 public class CategoriesDao {
-    private List<Category> categoriesList = new ArrayList<>();
+    private final JdbcTemplate jdbcTemplate;
+
+    public List<Category> getAllCategories() {
+        String sql = "select * from categories";
+        return jdbcTemplate.query(sql,new BeanPropertyRowMapper<>(Category.class));
+    }
+
 }
