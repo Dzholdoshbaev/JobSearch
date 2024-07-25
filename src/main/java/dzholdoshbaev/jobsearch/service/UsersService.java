54806093336b9bfed1beaf3471f5b3c621d628f2
abdyrakhman <medzholdoshbaev@gmail.com>
2024-07-25 22:47:09 +0600

    added in server User impl

--- src/main/java/dzholdoshbaev/jobsearch/service/UsersService.java
+++ src/main/java/dzholdoshbaev/jobsearch/service/UsersService.java
@@ -2,6 +2,18 @@ package dzholdoshbaev.jobsearch.service;
 
 import dzholdoshbaev.jobsearch.dto.UsersDto;
 
+import java.util.List;
+
 public interface UsersService {
     void createUser(UsersDto usersDto);
+
+    List<UsersDto> getAllUsers();
+
+    UsersDto getUserByName(String name);
+
+    UsersDto getUserByPhoneNumber(String phoneNumber);
+
+    UsersDto getUserByEmail(String email);
+
+    Boolean checkUserByEmail(String email);
 }
