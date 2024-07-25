3d29dcca28e591cedd756c64248aec801ccbfcdb
abdyrakhman <medzholdoshbaev@gmail.com>
2024-07-25 16:01:50 +0600

    created SQL request methods for EducationInfoDao

--- src/main/java/dzholdoshbaev/jobsearch/dao/EducationInfoDao.java
+++ src/main/java/dzholdoshbaev/jobsearch/dao/EducationInfoDao.java
@@ -2,13 +2,21 @@ package dzholdoshbaev.jobsearch.dao;
 
 import dzholdoshbaev.jobsearch.model.EducationInfo;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Data
+@RequiredArgsConstructor
 public class EducationInfoDao {
-    private List<EducationInfo> educationInfoList= new ArrayList<>();
+    private final JdbcTemplate jdbcTemplate;
+
+    public List<EducationInfo> getAllEducationInfo() {
+        String sql = "select * from education_info";
+        return jdbcTemplate.query(sql,new BeanPropertyRowMapper<>(EducationInfo.class));
+    }
 }
