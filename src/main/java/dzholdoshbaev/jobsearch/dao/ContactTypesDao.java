2e2eba13774274fe59294d67983741710d63d12b
abdyrakhman <medzholdoshbaev@gmail.com>
2024-07-25 16:01:41 +0600

    created SQL request methods for ContactTypesDao

--- src/main/java/dzholdoshbaev/jobsearch/dao/ContactTypesDao.java
+++ src/main/java/dzholdoshbaev/jobsearch/dao/ContactTypesDao.java
@@ -2,13 +2,21 @@ package dzholdoshbaev.jobsearch.dao;
 
 import dzholdoshbaev.jobsearch.model.ContactTypes;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Data
+@RequiredArgsConstructor
 public class ContactTypesDao {
-    private List<ContactTypes> contactTypesList = new ArrayList<>();
+    private final JdbcTemplate jdbcTemplate;
+
+    public List<ContactTypes> getAllContactTypes() {
+        String sql = "select * from contact_types";
+        return jdbcTemplate.query(sql,new BeanPropertyRowMapper<>(ContactTypes.class));
+    }
 }
