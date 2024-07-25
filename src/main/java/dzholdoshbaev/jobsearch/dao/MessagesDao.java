aaa2dfc3de068bdd1885b17fa53283ab2e71bcce
abdyrakhman <medzholdoshbaev@gmail.com>
2024-07-25 16:01:57 +0600

    created SQL request methods for MessagesDao

--- src/main/java/dzholdoshbaev/jobsearch/dao/MessagesDao.java
+++ src/main/java/dzholdoshbaev/jobsearch/dao/MessagesDao.java
@@ -2,13 +2,21 @@ package dzholdoshbaev.jobsearch.dao;
 
 import dzholdoshbaev.jobsearch.model.Messages;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Data
+@RequiredArgsConstructor
 public class MessagesDao {
-    private List<Messages> messagesList = new ArrayList<>();
+    private final JdbcTemplate jdbcTemplate;
+
+    public List<Messages> getAllMessages() {
+        String sql = "select * from messages";
+        return jdbcTemplate.query(sql,new BeanPropertyRowMapper<>(Messages.class));
+    }
 }
