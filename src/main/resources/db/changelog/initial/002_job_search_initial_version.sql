d2277b9baa6a44b5bb23c0b566dfde7dfb883758
abdyrakhman <medzholdoshbaev@gmail.com>
2024-07-29 15:37:50 +0600

    corrected date and timestamp and finished migration

--- src/main/resources/db/changelog/initial/002_job_search_initial_version.sql
+++ src/main/resources/db/changelog/initial/002_job_search_initial_version.sql
@@ -10,12 +10,12 @@ VALUES ('adilet', 'urmat',47, 'urmat@gmail.com', 'password1','0555433444', 'empl
        ('alibek', 'bek',35, 'alibek@gmail.com', 'password2','0555433443', 'applicant'),
        ('mirbek', 'alishev',45, 'mirbek@gmail.com', 'password3','0555433447', 'applicant');
 
-INSERT INTO resumes (applicant_id, name, category_id, salary, is_active)
-VALUES (2, 'Resume 1', 1, 2800.00, TRUE),
-       (3, 'Resume 2', 2, 3200.00, TRUE);
+INSERT INTO resumes (applicant_id, name, category_id, salary, is_active,created_date)
+VALUES (2, 'Resume 1', 1, 2800.00, TRUE,current_date),
+       (3, 'Resume 2', 2, 3200.00, TRUE,current_date);
 
-INSERT INTO vacancies (name, description, category_id, salary, exp_from, exp_to, is_active, author_id)
-VALUES ('Job 1', 'Description for Job 1', 1, 3000.00, 2, 5, TRUE, 1);
+INSERT INTO vacancies (name, description, category_id, salary, exp_from, exp_to, is_active, author_id,created_date)
+VALUES ('Job 1', 'Description for Job 1', 1, 3000.00, 2, 5, TRUE, 1,current_date);
 
 INSERT INTO responded_applicants (resume_id, vacancy_id, confirmation)
 VALUES (1, 1, TRUE),
@@ -29,9 +29,9 @@ INSERT INTO contacts_info (type_id, resume_id, info)
 VALUES (1, 1, 'alibek@example.com'),
        (1, 2, 'mirbek@example.com');
 
-INSERT INTO messages (responded_applicants_id, content)
-VALUES (1, 'Message content for responded applicant 1'),
-       (2, 'Message content for responded applicant 2');
+INSERT INTO messages (responded_applicants_id, content,timestamp)
+VALUES (1, 'Message content for responded applicant 1',current_timestamp()),
+       (2, 'Message content for responded applicant 2',current_timestamp());
 
 INSERT INTO education_info (resume_id, institution, program, start_date, end_date, degree)
 VALUES (1, 'University A', 'Computer Science', '2015-09-01', '2019-06-30', 'Bachelor'),
