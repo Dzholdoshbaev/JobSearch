b8570a5a83be9ee6362df66d3d86bd2ecacc3d62
abdyrakhman <medzholdoshbaev@gmail.com>
2024-08-16 17:00:15 +0600

    added authorities controller

---
+++ src/main/java/dzholdoshbaev/jobsearch/controller/AuthController.java
@@ -0,0 +1,25 @@
+package dzholdoshbaev.jobsearch.controller;
+
+import dzholdoshbaev.jobsearch.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UsersService usersService;
+
+    @GetMapping("/login")
+    public String login() {
+        return "profile/login";
+    }
+
+    @PostMapping("/login")
+    public String loginPost() {
+        return "redirect:/profile";
+    }
+}
