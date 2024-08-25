49eace2506e33adf674759475b7c2b466ec6b6c7
abdyrakhman <medzholdoshbaev@gmail.com>
2024-08-25 19:09:43 +0600

    corected for resume

--- src/main/java/dzholdoshbaev/jobsearch/repository/WorkExperienceInfoRepository.java
+++ src/main/java/dzholdoshbaev/jobsearch/repository/WorkExperienceInfoRepository.java
@@ -6,4 +6,6 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface WorkExperienceInfoRepository extends JpaRepository<WorkExperienceInfo, Long> {
+    WorkExperienceInfo findByResumes_Id(Long resumeId);
+
 }
