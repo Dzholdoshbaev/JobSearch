87b3c4ab7c7424a69ca90fc0d84f2eafdea5e35b
abdyrakhman <medzholdoshbaev@gmail.com>
2024-08-30 17:03:03 +0600

    delete unuse imports

--- src/main/java/dzholdoshbaev/jobsearch/controller/CategoriesController.java
+++ src/main/java/dzholdoshbaev/jobsearch/controller/CategoriesController.java
@@ -1,25 +1,11 @@
 package dzholdoshbaev.jobsearch.controller;
 
-import dzholdoshbaev.jobsearch.service.CategoriesService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/categories")
 @RequiredArgsConstructor
 public class CategoriesController {
-
-    private final CategoriesService categoriesService;
-//
-//    @GetMapping
-//   public ResponseEntity<List<CategoriesDto>> getCategories() {
-//       return new ResponseEntity<>(categoriesService.getCategories(), HttpStatus.OK);
-//   }
-
 }
