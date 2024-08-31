dc7d3b8b91b5b97e5dee4187a8daf151e86ed15e
abdyrakhman <medzholdoshbaev@gmail.com>
2024-08-31 09:24:58 +0600

    corrected  exceptions

--- src/main/java/dzholdoshbaev/jobsearch/errors/GlobalControllerAdvice.java
+++ src/main/java/dzholdoshbaev/jobsearch/errors/GlobalControllerAdvice.java
@@ -4,11 +4,14 @@ import dzholdoshbaev.jobsearch.service.ErrorService;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
+import java.nio.file.AccessDeniedException;
 import java.util.NoSuchElementException;
 
 @ControllerAdvice
@@ -30,6 +33,39 @@ public class GlobalControllerAdvice {
         model.addAttribute("status", HttpStatus.BAD_REQUEST.value());
         model.addAttribute("reason", HttpStatus.BAD_REQUEST.getReasonPhrase());
         model.addAttribute("details" , request);
+        return "errors/error";
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public String accessDenied(HttpServletRequest request, Model model) {
+        model.addAttribute("status", HttpStatus.FORBIDDEN.value());
+        model.addAttribute("reason", HttpStatus.FORBIDDEN.getReasonPhrase());
+        model.addAttribute("details", request);
+        return "errors/error";
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public String httpMessageNotReadable(HttpServletRequest request, Model model) {
+        model.addAttribute("status", HttpStatus.BAD_REQUEST.value());
+        model.addAttribute("reason", HttpStatus.BAD_REQUEST.getReasonPhrase());
+        model.addAttribute("details", request);
+        return "errors/error";
+    }
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public String noHandlerFound(HttpServletRequest request, Model model) {
+        model.addAttribute("status", HttpStatus.NOT_FOUND.value());
+        model.addAttribute("reason", HttpStatus.NOT_FOUND.getReasonPhrase());
+        model.addAttribute("details", request);
+        return "errors/error";
+    }
+
+    @ExceptionHandler(Exception.class)
+    public String handleException(HttpServletRequest request, Model model, Exception e) {
+        model.addAttribute("status", HttpStatus.INTERNAL_SERVER_ERROR.value());
+        model.addAttribute("reason", HttpStatus.INTERNAL_SERVER_ERROR.getReasonPhrase());
+        model.addAttribute("details", request);
+        model.addAttribute("exception", e.getMessage());
         return "errors/error";
     }
 }
