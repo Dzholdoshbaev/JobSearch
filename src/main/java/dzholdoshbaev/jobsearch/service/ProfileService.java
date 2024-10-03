d02869cd8d4c10b051377966a3aee2a0e978d676
abdyrakhman <medzholdoshbaev@gmail.com>
2024-10-03 16:21:35 +0600

    delete un use controller

--- src/main/java/dzholdoshbaev/jobsearch/service/ProfileService.java
+++
@@ -1,4 +0,0 @@
-package dzholdoshbaev.jobsearch.service;
-
-public interface ProfileService {
-}
--- src/main/java/dzholdoshbaev/jobsearch/service/impl/ProfileServiceImpl.java
+++
@@ -1,6 +0,0 @@
-package dzholdoshbaev.jobsearch.service.impl;
-
-import dzholdoshbaev.jobsearch.service.ProfileService;
-
-public class ProfileServiceImpl implements ProfileService {
-}
